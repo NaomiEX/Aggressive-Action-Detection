7b5eb53a7afbd1eefa80abca2393a02348661f74
Ci Leong Ong <ongcileong2001@gmail.com>
2022-09-25 01:28:46 +0800

    Add logic for tying together frames into a video

--- img_utils.py
+++ img_utils.py
@@ -36,6 +36,35 @@ def folder_to_imgs(folder_path, img_extension):
     # the desired type.
     path = os.path.join(folder_path, f"*{img_extension}")
 
+    filenames = sorted(glob.glob(path))
+
     # Return an iterator to the images read.
     # TODO Check whether the images yielded are in correct order.
-    return map(cv2.imread, glob.iglob(path))
+    return map(cv2.imread, filenames)
+
+
+def imgs_to_vid(filename, dims, fps, images):
+    """
+    Parameters:
+    filename : The name of the video output, must end with ".mp4".
+    dims     : A tuple (width, height) specifying the video dimensions.
+    fps      : Frames per second.
+    images   : An iterator to OpenCV Mat image objects.
+    """
+    # Function limitation: can only output videos in mp4 codec.
+    assert filename.endswith(".mp4"), "Attempted to output non mp4-codec video."
+
+    # Create a OpenCV VideoWriter object as IO wrapper.
+    writer = cv2.VideoWriter(
+        filename,
+        cv2.VideoWriter_fourcc(*"mp4v"),
+        fps,
+        dims
+    )
+
+    # Write the image into the video container.
+    for image in images:
+        writer.write(image)
+
+    # Clean up to prevent memory leaks.
+    writer.release()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import glob
+from math import log10
 from img_utils import *
 
 
@@ -12,13 +13,32 @@ from img_utils import *
 IMGS_DIR = "./images"
 
 capture = cv2.VideoCapture("./project.mp4")
+
+# Extract video metadata.
+frame_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
 fps = capture.get(cv2.CAP_PROP_FPS)
 
 images = stream_to_imgs(capture)
-filename_pat = os.path.join(IMGS_DIR, "image{}.jpg")
+
+filename_pat = os.path.join(IMGS_DIR, "image_{:0{}d}.jpg")
+id_length = max(int(log10(frame_count)) + 1, 1)
 
 for _id, image in enumerate(images, start=1):
-    filename = filename_pat.format(_id)
+    filename = filename_pat.format(_id, id_length)
     cv2.imwrite(filename, image)
 
+    # Keep track of the video dimensions.
+    height, width, _ = image.shape
+    dims = (width, height)
+
 capture.release()
+
+
+
+
+
+# Step 3: Tie the frames back into a video.
+
+images = folder_to_imgs(IMGS_DIR, ".jpg")
+
+imgs_to_vid("new_project.mp4", dims, fps, images)
